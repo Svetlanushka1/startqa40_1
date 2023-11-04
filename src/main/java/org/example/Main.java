b6fa37d85e995eec530baace0f402bc4ff812fae
Svetlanushka1 <durovasvetlanushka@gmail.com>
2023-11-04 20:28:47 +0200

    Created new branch and changed main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("First project has be cloned successfully from GIT to VM");
+        System.out.println("");
+        System.out.println("????");
     }
 }
